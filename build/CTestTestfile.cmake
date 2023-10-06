6a80487f4cdbcf105d7863d2f534fe37ce260109
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-10-06 11:50:39 +1030

    Add files via upload
    Dumping for what was my use of google test, will come back to it later

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/maxtwelftree/Documents/Work/2023/Semester 2/COMP SCI 1102/Assignments /Major Project/Painter.IO/Code/googletest/googletest
+# Build directory: /Users/maxtwelftree/Documents/Work/2023/Semester 2/COMP SCI 1102/Assignments /Major Project/Painter.IO/Code/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
