dbff73696a1bbef3369b3863d2612a3334df966b
vas-byte <vasmichalakis@hotmail.com>
2023-10-06 11:43:23 +1030

    Map Loads from JSON

--- Implementations/Game.cpp
+++ Implementations/Game.cpp
@@ -1,18 +1,43 @@
 #include "../Headers/Game.h"
-
 #include <iostream>
 #include <vector>
-
 #include "../Headers/Bullet.h"
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 Game::Game() {
   width = 1440;
   height = 800;
   map = TileMap();
 
+  //init JSON object
+  nlohmann::json j;
+  std::ifstream ifs("State/map.json");
+
+  //open JSON file
+  if (!ifs.is_open())
+    {
+        //return false;;
+    }
+
+  //Write file to JSON object
+  ifs >> j;
+
+  //Close file IO
+  ifs.close();
+
   // define the level with an array of tile indices
   int level[4500];
-  std::fill_n(level, 4500, 6);
+
+  //index in array for map (tile indicies)
+  int i = 0;
+
+  //Loads map from JSON file with list of tile indices
+  for (int elem : j["map"]){
+    level[i] = elem;
+    i++;
+  }
+      
   map.load("Assets/Tileset.png", sf::Vector2u(16, 16), level, 90, 50);
 }
 
@@ -26,13 +51,12 @@ void Game::set_bullet(Bullet* bullet) { bullets.push_back(bullet); }
 
 void Game::show_bullet(sf::RenderWindow& app) {
   for (int i = 0; i < bullets.size(); i++) {
-
     if (!bullets.at(i)->isInsideMap(width, height)) {
       delete bullets.at(i);
       bullets.erase(bullets.begin() + i);
       continue;
     }
-    
+
     app.draw(bullets.at(i)->shootBullet());
   }
 }
--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-CPPFLAGS = -I/opt/homebrew/Cellar/sfml/2.6.0/include 
+CPPFLAGS = -I/opt/homebrew/Cellar/sfml/2.6.0/include -I/opt/homebrew/Cellar/nlohmann-json/3.11.2/include
 LDFLAGS = -L/opt/homebrew/Cellar/sfml/2.6.0/lib -lsfml-graphics -lsfml-window -lsfml-system
 INCLUDE_PATH = /Implementations
+CXXFLAGS = -std=c++17
 
 default: main.cpp $(wildcard $(INCLUDE_PATH)/*.cpp)
-	g++ $(CPPFLAGS) $(LDFLAGS) main.cpp Implementations/Bullet.cpp Implementations/Collectable.cpp Implementations/Game.cpp Implementations/Gun.cpp Implementations/Object.cpp Implementations/Person.cpp Implementations/Player.cpp Implementations/tileMap.cpp -o painter.IO
+	g++ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS) main.cpp Implementations/Bullet.cpp Implementations/Collectable.cpp Implementations/Game.cpp Implementations/Gun.cpp Implementations/Object.cpp Implementations/Person.cpp Implementations/Player.cpp Implementations/tileMap.cpp -o painter.IO
 
 run: main.cpp $(wildcard $(INCLUDE_PATH)/*.cpp)
 	make default
